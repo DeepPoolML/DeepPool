568c35c5518014eac921984de3b8a51861693151
Seo Jin Park <seojin@csail.mit.edu>
2021-07-30 12:29:39 -0400

    Skeleton for CommunicationHandler interface.

--- csrc/communication.h
+++ csrc/communication.h
@@ -15,7 +15,46 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
+
+
 class CommunicationHandler {
+ public:
+  /**
+   * Constructs communicationHandler base class.
+   * 
+   * \param worldSize   Number of ranks.
+   * \param tensorTags  Mapping from xferName to p2p communication tag.
+   * \param rank        Rank of the current node.
+   * \param jobRankToGlobalRank   Mapping from job's internal rank to cluster rank.
+   * \param tensorInCuda  tensor given to send/recv methods are cuda tensors (false if CPU tensor).
+   */
+  CommunicationHandler(int worldSize, json tensorTags, int rank,
+      json jobRankToGlobalRank, bool tensorInCuda = true);
+
+  /**
+   * Changes from Python runtime.
+   * - Compute tensor dimension from json spec.
+   * - recv takes empty tensor that is ready to be filled.
+   * - No separate async/sync methods.
+   * 
+   * Undecided: take tensorName or tag? maybe just take tag? It may not be
+   * that difficult to save the tag in runnableModule's layer..?
+   */
+  virtual void send(const torch::Tensor& tensor, int tag, int dest,
+                    bool async = false);
+  virtual void recv(torch::Tensor& tensor, int tag, int src,
+                    bool async = false);
+  
+  /**
+   * Returns the tag for p2p communication send/recv.
+   *
+   * \param xferName  Transfer name specificed in spec. Sender and receiver
+   *                  should use the same xferName.
+   */
+  int getTag(const std::string& xferName);
+};
+
+class CommunicationHandlerNCCL : public CommunicationHandler {
 };
 
 #endif
